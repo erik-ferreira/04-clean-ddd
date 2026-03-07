d17f9ca006b18b8fac59f08e9cc5e429a608aca8
erik-ferreira <erikdesouzagf@gmail.com>
2026-03-07 00:48:38 -0300

    create use case to delete question

---
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Question } from "@/domain/forum/enterprise/entities/question";
+import { QuestionsRepository } from "../repositories/questions-repository";
+
+interface DeleteQuestionUseCaseRequest {
+  authorId: string;
+  questionId: string;
+}
+
+interface DeleteQuestionUseCaseResponse {}
+
+export class DeleteQuestionUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+  }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId);
+
+    if (!question) {
+      throw new Error("Question not found.");
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error("Not Allowed.");
+    }
+
+    await this.questionsRepository.delete(question);
+
+    return {};
+  }
+}
--- teste/factories/make-question.ts
+++ teste/factories/make-question.ts
@@ -6,13 +6,19 @@ import {
 } from "@/domain/forum/enterprise/entities/question";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: faker.lorem.sentence(),
-    content: faker.lorem.text(),
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 
   return question;
 }
