cb16bc831adc1b708fa3430870f9eb152e558123
erik-ferreira <erikdesouzagf@gmail.com>
2026-03-11 00:45:11 -0300

    create use case to fetch recent questions

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -0,0 +1,45 @@
+import { FetchRecentQuestionCase } from "./fetch-recent-questions";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+import { makeQuestion } from "teste/factories/make-question";
+import { InMemoryQuestionsRepository } from "teste/in-memory-question-repository";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: FetchRecentQuestionCase;
+
+describe("Fetch Recent Question", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new FetchRecentQuestionCase(inMemoryQuestionsRepository);
+  });
+
+  it("should be able to delete a question", async () => {
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date(2026, 0, 20) }),
+    );
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date(2026, 0, 18) }),
+    );
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date(2026, 0, 23) }),
+    );
+
+    const { questions } = await sut.execute({ page: 1 });
+
+    expect(questions).toEqual([
+      expect.objectContaining({ createdAt: new Date(2026, 0, 23) }),
+      expect.objectContaining({ createdAt: new Date(2026, 0, 20) }),
+      expect.objectContaining({ createdAt: new Date(2026, 0, 18) }),
+    ]);
+  });
+
+  it("should be able to fetch paginated recent questions", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionsRepository.create(makeQuestion());
+    }
+
+    const { questions } = await sut.execute({ page: 2 });
+
+    expect(questions).toHaveLength(2);
+  });
+});
---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,24 @@
+import { Question } from "@/domain/forum/enterprise/entities/question";
+
+import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
+import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
+
+interface FetchRecentQuestionCaseRequest {
+  page: number;
+}
+
+interface FetchRecentQuestionCaseResponse {
+  questions: Question[];
+}
+
+export class FetchRecentQuestionCase {
+  constructor(private questionRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionCaseRequest): Promise<FetchRecentQuestionCaseResponse> {
+    const questions = await this.questionRepository.findManyRecent({ page });
+
+    return { questions };
+  }
+}
