abd3c50a1e6b929c5d88f77bbd3f6e7b4a9d50ec
erik-ferreira <erikdesouzagf@gmail.com>
2026-03-12 23:44:33 -0300

    create comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,37 @@
+import { CommentOnQuestionUseCase } from "./comment-on-question";
+
+import { InMemoryQuestionsRepository } from "teste/in-memory-question-repository";
+import { InMemoryQuestionCommentsRepository } from "teste/in-memory-question-comments-repository";
+import { makeQuestion } from "teste/factories/make-question";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionsCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: CommentOnQuestionUseCase;
+
+describe("Comment on Question", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryQuestionsCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionsCommentsRepository,
+    );
+  });
+
+  it("should be able to comment on question", async () => {
+    const question = makeQuestion();
+
+    await inMemoryQuestionsRepository.create(question);
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: "Comentário teste",
+    });
+
+    expect(inMemoryQuestionsCommentsRepository.items[0].content).toEqual(
+      "Comentário teste",
+    );
+  });
+});
