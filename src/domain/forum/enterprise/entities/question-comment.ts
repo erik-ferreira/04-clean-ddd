8c611dbc14c53f6d97c86fd8fd500cd6b862f292
erik-ferreira <erikdesouzagf@gmail.com>
2026-03-12 23:12:11 -0300

    create entities comments

---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,54 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+import { Optional } from "@/core/types/optional";
+
+export interface AnswerCommentProps {
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class AnswerComment extends Entity<AnswerCommentProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(newContent: string) {
+    this.props.content = newContent;
+    this.touch();
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, "createdAt">,
+    id?: UniqueEntityID,
+  ) {
+    const question = new AnswerComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    );
+
+    return question;
+  }
+}
