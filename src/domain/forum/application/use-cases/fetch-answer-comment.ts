9415aac0aa697f094acb4f011d19c89ddd61c5ab
erik-ferreira <erikdesouzagf@gmail.com>
2026-03-17 23:08:37 -0300

    create use case to list comments of answer

---
+++ src/domain/forum/application/use-cases/fetch-answer-comment.ts
@@ -0,0 +1,28 @@
+import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+
+import { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
+
+interface FetchAnswerCommentsUseCaseRequest {
+  answerId: string;
+  page: number;
+}
+
+interface FetchAnswerCommentsUseCaseResponse {
+  answerComments: AnswerComment[];
+}
+
+export class FetchAnswerCommentsUseCase {
+  constructor(private answerCommentRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    answerId,
+    page,
+  }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
+    const answerComments =
+      await this.answerCommentRepository.findManyByAnswerId(answerId, {
+        page,
+      });
+
+    return { answerComments };
+  }
+}
